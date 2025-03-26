b4cf8c6fe8c9f7548926413207050eef804a09b7
Alison-Tierris <alison.tierris@ark-isp.com.br>
2025-03-26 11:31:26 -0300

    correções

--- backend/app/routes/veiculos.py
+++ backend/app/routes/veiculos.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from app.database import get_db
 from app.models import Veiculo
-from app.schemas import VeiculoCreate, VeiculoUpdate, VeiculoResponse
+from app.schemas import VeiculoCreate, VeiculoResponse #, VeiculoUpdate
 from app.utils.auth import get_current_user
 from app.models.user import users
 
@@ -30,19 +30,19 @@ async def obter_veiculo(veiculo_id: int, db: AsyncSession = Depends(get_db), usu
         raise HTTPException(status_code=404, detail="Veículo não encontrado")
     return veiculo
 
-@router.put("/{veiculo_id}", response_model=VeiculoResponse)
-async def atualizar_veiculo(veiculo_id: int, dados: VeiculoUpdate, db: AsyncSession = Depends(get_db), usuario: Usuario = Depends(get_current_user)):
-    result = await db.execute(select(Veiculo).where(Veiculo.id == veiculo_id, Veiculo.usuario_id == usuario.id))
-    veiculo = result.scalar_one_or_none()
-    if not veiculo:
-        raise HTTPException(status_code=404, detail="Veículo não encontrado ou não autorizado")
+# @router.put("/{veiculo_id}", response_model=VeiculoResponse)
+# async def atualizar_veiculo(veiculo_id: int, dados: VeiculoUpdate, db: AsyncSession = Depends(get_db), usuario: Usuario = Depends(get_current_user)):
+#     result = await db.execute(select(Veiculo).where(Veiculo.id == veiculo_id, Veiculo.usuario_id == usuario.id))
+#     veiculo = result.scalar_one_or_none()
+#     if not veiculo:
+#         raise HTTPException(status_code=404, detail="Veículo não encontrado ou não autorizado")
 
-    for campo, valor in dados.dict(exclude_unset=True).items():
-        setattr(veiculo, campo, valor)
+#     for campo, valor in dados.dict(exclude_unset=True).items():
+#         setattr(veiculo, campo, valor)
 
-    await db.commit()
-    await db.refresh(veiculo)
-    return veiculo
+#     await db.commit()
+#     await db.refresh(veiculo)
+#     return veiculo
 
 @router.delete("/{veiculo_id}")
 async def deletar_veiculo(veiculo_id: int, db: AsyncSession = Depends(get_db), usuario: Usuario = Depends(get_current_user)):
